b6f90c671e776c766737a76d8b7539308107ff9b
Roshan <roshan.kouwenhoven@student.hu.nl>
2025-09-15 15:24:53 +0200

    commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# import random
+#
+# boek = 24.95
+# korting = 0.6
+# verzenden1 = 3
+# verzenden2 = 0.75
+# aantal = 60
+#
+# if aantal > 1 : totaalVer = aantal * verzenden2 - verzenden2 + verzenden1
+# if aantal == 1: totaalVer = aantal + verzenden1
+# totaalBedrag = (boek * korting) * aantal + totaalVer
+# #print( "Totaal Bedrag:", round(totaalBedrag, 3) )
+#
+# var1 = 10.3
+# var2 = 17.4
+# var3 = 3.7
+#
+# gemiddelde = (var1 + var2 + var3) / 3
+# #print( "Gemiddelde:", round(gemiddelde, 3) )
+# #print( "Grootste getal: ", max(var1, var2, var3) )
+# #print( "Kleinste getal:", min(var1, var2, var3) )
+#
+# s1 = "Hogeschool Utrecht"
+# s2 = "Heidelberglaan"
+# s3 = "Programming"
+#
+# #print("a)","sch" in s1 )
+# #print("b)"," " not in s2 )
+# #print("c)", s1 + s2 + s3 )
+# #print("d)", s2[12] )
+# #print("e)", s1[12] == s3[8] )
+# #print("f)", len( 20 * s3 ) > 200 )
+#
+# #temp = int( input("Wat is de temperatuur? ") )
+# #tempFahr = 1.8 * temp + 32
+# #print("Het is momenteel " + str(temp) + " graden celcius.")
+# #print("Het is momenteel " + str(tempFahr) + " graden fahrenheit.")
+#
+# #naam = input("Wat is uw naam? ")
+# #leeftijd = int( input("Wat is uw leeftijd? ") )
+# #print("Hallo " + naam + ", U bent over 1 jaar", (int(leeftijd + 1) ) )
+#
+# a = random.randint(1, 6)
+# b = random.randint(1, 6)
+# print("Dobbelsteen A) " + str(a) + " Dobbelsteen B) " + str(b) )
+# if a == b : print("Beide dobbelstenen zijn gelijk.")
+# else: print("Ze zijn niet het zelfde.")
---
+++ test.py
@@ -0,0 +1,11 @@
+def gemiddelde(var1, var2):
+    resultaat = (var1 + var2) / 2
+    return resultaat
+
+
+
+inp1 = input("Voer een getal in: ")
+inp2 = input("Voer een getal in: ")
+inp1 = float(inp1)
+inp2 = float(inp2)
+print(gemiddelde(inp1, inp2))
