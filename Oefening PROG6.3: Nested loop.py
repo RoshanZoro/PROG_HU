db185b75f5aeff28962d0f350ed4d8991993f21d
Roshan <roshan.kouwenhoven@student.hu.nl>
2025-09-22 16:35:52 +0200

    comments

--- Oefening PROG6.3: Nested loop.py
+++ Oefening PROG6.3: Nested loop.py
@@ -2,3 +2,7 @@ for getal in range(1,11):
     for i in range (1,11):
         print(f"{getal} x {i} = {getal*i}")
     print()
+#buitenste loop is een urenwijzer, binnenste loop de minutenwijzer,
+#de minutenwijzer moet eerst helemaal rondgaan, dan pas gaat
+#de urenwijzer 1 stap verder
+#dus de eerste for loop wacht tot de tweede klaar is
