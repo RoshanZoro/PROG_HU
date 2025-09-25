299369f4fdff5f448ed1beda6d7b914b918cd9ac
Roshan <roshan.kouwenhoven@student.hu.nl>
2025-09-25 15:14:52 +0200

    opdracht

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-def gemiddelde(var1, var2):
-    resultaat = (var1 + var2) / 2
-    return resultaat
-
-
-
-inp1 = input("Voer een getal in: ")
-inp2 = input("Voer een getal in: ")
-inp1 = float(inp1)
-inp2 = float(inp2)
-print(gemiddelde(inp1, inp2))
-
+telefoonboek = { 'Roshan': "0640780297", "hulpdiensten": "112"}
+naam = input("Wie zoekt u? ")
+nummer = telefoonboek.get(naam, "werd niet gevonden")
+print(f"Het nummer van {naam}: {nummer}")
+zoeknummer = input("Bij welk nummer zoekt u een naam?")
+if zoeknummer not in telefoonboek.values():
+    print("Dit nummer is onbekend!")
+else:
+    for naam, nummer in telefoonboek.items():
+        if nummer == zoeknummer:
+            print(f"Dit nummer is van {naam}")
